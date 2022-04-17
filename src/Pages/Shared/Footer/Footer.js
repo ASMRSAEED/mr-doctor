5b8b92947b65e6dcb41ba6f26943a772685f9ff3
ASMRSAEED <asmrsaeed@gmail.com>
2022-04-18 02:04:59 +0600

    Component Created

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <Footer>
+            <p> <small>copyright text here</small> </p>
+        </Footer>
+    );
+};
+
+export default Footer;
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+      <Header>
+          <p>This is Header</p>
+      </Header>
+    );
+};
+
+export default Header;
